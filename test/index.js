3359765a0c28e36ea63df53e75fc0b21db8b18dd
chencheng <cheng.chen@tianjishuju.com>
2018-05-31 08:37:40 +0800

    first commit

--- test/index.js
+++ test/index.js
@@ -1,10 +1,19 @@
-const unclassisfied = require('../src/unclassified');
-
-const a_fn = (params) => {
-
+const testUnclassisfied = () => {
+    const unclassisfied = require('../src/unclassified');
+    const a_fn = (params) => {};
+    const render = unclassisfied.throttle(a_fn, 1600, null);
+    render(1);
+    render(2);
 }
 
-const render = unclassisfied.throttle(a_fn, 1600, null);
+const testStringSet = () => {
+    const StringSet = require('../src/string_set');
 
-render(1);
-render(2);
+    const stringSet = new StringSet("chengc1");
+    console.log(stringSet._items)
+    console.log(stringSet._nums)
+    console.log(stringSet.values());
+}
+
+
+testStringSet();
